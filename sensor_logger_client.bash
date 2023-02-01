d2f702ee2e7ee7916ea892976f8c9d9c30453364
Kyuunex <kyuunex@protonmail.ch>
2023-02-01 11:12:50 +0400

    update client

--- sensor_logger_client.bash
+++ sensor_logger_client.bash
@@ -1,6 +1,21 @@
 #!/bin/bash
+
+IP_AND_PORT=$@
+
+if ! command -v sensors &> /dev/null
+then
+    echo "lm-sensors is not installed!"
+    exit
+fi
+
+if ! command -v curl &> /dev/null
+then
+    echo "curl is not installed!"
+    exit
+fi
+
 while :
 do
-    curl -d "$(sensors -j)" -H "Content-Type: application/json" -X POST "http://192.168.6.200:3000/sensor_logger"
+    curl -d "$(sensors -j)" -H "Content-Type: application/json" -X POST "http://$IP_AND_PORT/sensor_logger"
     sleep 0.2
 done
